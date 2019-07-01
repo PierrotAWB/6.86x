1374e8c6fd185196089c394d614c454544d310dc
PierrotAWB <andrewwang298@gmail.com>
2019-07-01 18:22:44 -0400

    Do feature engineering

--- project1/sentiment_analysis/project1.py
+++ project1/sentiment_analysis/project1.py
@@ -376,11 +376,14 @@ def bag_of_words(texts):
     Feel free to change this code as guided by Problem 9
     """
     # Your code here
+    with open('stopwords.txt', 'r') as input:
+        stopwords = list(map(str, input.read().split("\n")))
+
     dictionary = {} # maps word to unique index
     for text in texts:
         word_list = extract_words(text)
         for word in word_list:
-            if word not in dictionary:
+            if word not in dictionary and word not in stopwords:
                 dictionary[word] = len(dictionary)
     return dictionary
 #pragma: coderesponse end
@@ -406,7 +409,7 @@ def extract_bow_feature_vectors(reviews, dictionary):
         word_list = extract_words(text)
         for word in word_list:
             if word in dictionary:
-                feature_matrix[i, dictionary[word]] = 1
+                feature_matrix[i, dictionary[word]] += 1
     return feature_matrix
 #pragma: coderesponse end
 
